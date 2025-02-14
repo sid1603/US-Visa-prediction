afde3f14b4ea92b266f4960d3a88efcd2bc88c92
sid1603 <sidd.priyadarshi687@gmail.com>
2025-02-14 21:26:18 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US_visa",
+    version="0.0.0",
+    author="Sid",
+    author_email="sidd.priyadarshi687@gmail.com",
+    packages=find_packages()
+)
